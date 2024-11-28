95250d7242cef19696db518f8003ee261a7a7b46
AmitGini <amitgini0369@gmail.com>
2024-11-28 14:36:41 +0200

    Reformat

--- Assets/Scripts/3-collisions/ScoreAdder.cs
+++ Assets/Scripts/3-collisions/ScoreAdder.cs
@@ -10,7 +10,7 @@ public class ScoreAdder : MonoBehaviour {
     [SerializeField] int pointsToAdd;
 
     private void OnTriggerEnter2D(Collider2D other) {
-        if (other.tag == triggeringTag && scoreField!=null) {
+        if (other.tag == triggeringTag && scoreField != null) {
             scoreField.AddNumber(pointsToAdd);
         }
     }
